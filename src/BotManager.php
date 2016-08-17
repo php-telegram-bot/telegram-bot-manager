55fcd6b53a7347fcd951a8fef7490b04aeecae85
Armando Lüscher <armando@noplanman.ch>
2016-08-17 14:19:28 +0200

    First commit!

---
+++ src/BotManager.php
@@ -0,0 +1,337 @@
+<?php
+/**
+ * This file is part of the TelegramBotManager package.
+ *
+ * (c) Armando Lüscher <armando@noplanman.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NPM\TelegramBotManager;
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
+use Longman\TelegramBot\Entities;
+
+/**
+ * Class BotManager.php
+ *
+ * Leave all member variables public to allow easy modification.
+ *
+ * @package NPM\TelegramBotManager
+ */
+class BotManager
+{
+    /**
+     * @var Telegram
+     */
+    public $telegram;
+
+    /**
+     * @var string The output for testing, instead of echoing
+     */
+    public $test_output;
+
+    /** vitals */
+    public $api_key;
+    public $botname;
+    public $secret;
+
+    public $action;
+    public $webhook;
+    public $selfcrt;
+
+	/**
+	 * BotManager constructor that assigns all necessary member variables.
+	 *
+	 * @param array $vars
+	 *
+	 * @throws \Exception
+	 */
+    public function __construct(array $vars)
+    {
+        if (!isset($vars['api_key'], $vars['botname'], $vars['secret'])) {
+            throw new \Exception('Some vital info is missing (api_key, botname or secret)');
+        }
+
+        // Set all important info.
+        foreach ($vars as $var => $value) {
+            $this->$var = $value;
+        }
+    }
+
+    /**
+     * Run this thing in all its glory!
+     *
+     * @throws \Exception
+     */
+    public function run()
+    {
+        // If this script is called via CLI, make it work just the same.
+        $this->makeCliFriendly();
+
+        // Initialise logging.
+        $this->initLogging();
+
+        // Make sure this is a valid call.
+        $this->validateSecret();
+
+        // Check for a valid action and set member variable.
+        $this->validateAndSetAction();
+
+        // Set up a new Telegram instance.
+        $this->telegram = new Telegram($this->api_key, $this->botname);
+
+        if ($this->isAction(['set', 'unset', 'reset'])) {
+            $this->validateAndSetWebhook();
+        } elseif ($this->isAction('handle')) {
+            // Set any extras.
+            $this->setBotExtras();
+            $this->handleRequest();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Allow this script to be called via CLI.
+     *
+     * $ php entry.php s=<secret> a=<action> l=<loop>
+     */
+    public function makeCliFriendly()
+    {
+        // If we're running from CLI, properly set $_GET.
+        if (PHP_SAPI === 'cli') {
+            // We don't need the first arg (the file name).
+            $args = array_slice($_SERVER['argv'], 1);
+
+            foreach ($args as $arg) {
+                @list($key, $val) = explode('=', $arg);
+                isset($key, $val) && $_GET[$key] = $val;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Initialise all loggers.
+     */
+    public function initLogging()
+    {
+        if (isset($this->logging) && is_array($this->logging)) {
+            foreach ($this->logging as $logger => $logfile) {
+                ('debug' === $logger) && TelegramLog::initDebugLog($logfile);
+                ('error' === $logger) && TelegramLog::initErrorLog($logfile);
+                ('update' === $logger) && TelegramLog::initUpdateLog($logfile);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Make sure the passed secret is valid.
+     *
+     * @throws \Exception
+     */
+    public function validateSecret()
+    {
+        $secretGet = isset($_GET['s']) ? (string)$_GET['s'] : '';
+        if (empty($this->secret) || $secretGet !== $this->secret) {
+            throw new \Exception('Invalid access');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Make sure the action is valid and set the member variable.
+     *
+     * @throws \Exception
+     */
+    public function validateAndSetAction()
+    {
+        $validActions = ['set', 'unset', 'reset', 'handle'];
+        $this->action = isset($_GET['a']) ? (string)$_GET['a'] : '';
+        if (!$this->isAction($validActions)) {
+            throw new \Exception('Invalid action');
+        }
+
+        return $this;
+    }
+
+    /**
+     * Make sure the webhook is valid and perform the requested webhook operation.
+     *
+     * @throws \Exception
+     */
+    public function validateAndSetWebhook()
+    {
+        if (empty($this->webhook) && $this->isAction(['set', 'reset'])) {
+            throw new \Exception('Invalid webhook');
+        }
+
+        if ($this->isAction(['unset', 'reset'])) {
+            $this->test_output = $this->telegram->unsetWebHook()->getDescription();
+        }
+        if ($this->isAction(['set', 'reset'])) {
+            $this->test_output = $this->telegram->setWebHook($this->webhook . '?a=handle&s=' . $this->secret, $this->selfcrt)->getDescription();
+        }
+
+        (@constant('PHPUNIT_TEST') !== true) && print($this->test_output . PHP_EOL);
+
+        return $this;
+    }
+
+    /**
+     * Check if the current action is one of the passed ones.
+     *
+     * @param $actions
+     *
+     * @return bool
+     */
+    public function isAction($actions)
+    {
+        // Make sure we're playing with an array without empty values.
+        $actions = array_filter((array)$actions);
+
+        return in_array($this->action, $actions, true);
+    }
+
+    /**
+     * Get the param of how long (in seconds) the script should loop.
+     *
+     * @return int
+     */
+    public function getLoopTime()
+    {
+        $loop_time = 0;
+
+        if (isset($_GET['l'])) {
+            $loop_time = (int)$_GET['l'];
+            if ($loop_time <= 0) {
+                $loop_time = 604800; // 7 days.
+            }
+        }
+
+        return $loop_time;
+    }
+
+    /**
+     * Handle the request, which calls either the Webhook or getUpdates method respectively.
+     *
+     * @throws \Exception
+     */
+    public function handleRequest()
+    {
+        if (empty($this->webhook)) {
+            if ($loop_time = $this->getLoopTime()) {
+                $this->handleGetUpdatesLoop($loop_time);
+            } else {
+                $this->handleGetUpdates();
+            }
+        } else {
+            $this->handleWebhook();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Loop the getUpdates method for the passed amount of seconds.
+     *
+     * @param $loop_time_in_seconds int
+     */
+    public function handleGetUpdatesLoop($loop_time_in_seconds)
+    {
+        // Remember the time we started this loop.
+        $now = time();
+
+        echo 'Looping getUpdates until ' . date('Y-m-d H:i:s', $now + $loop_time_in_seconds) . PHP_EOL;
+
+        while ($now > time() - $loop_time_in_seconds) {
+            $this->handleGetUpdates();
+
+            // Chill a bit.
+            sleep(2);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Handle the updates using the getUpdates method.
+     */
+    public function handleGetUpdates()
+    {
+        echo date('Y-m-d H:i:s', time()) . ' - ';
+
+        $response = $this->telegram->handleGetUpdates();
+        if ($response->isOk()) {
+            $results = array_filter((array)$response->getResult());
+
+            printf('Updates processed: %d' . PHP_EOL, count($results));
+
+            /** @var Entities\Update $result */
+            foreach ($results as $result) {
+                $chat_id = 0;
+                $text = 'Nothing';
+
+                $update_content = $result->getUpdateContent();
+                if ($update_content instanceof Entities\Message) {
+                    $chat_id = $update_content->getFrom()->getId();
+                    $text = $update_content->getText();
+                } elseif ($update_content instanceof Entities\InlineQuery || $update_content instanceof Entities\ChosenInlineResult) {
+                    $chat_id = $update_content->getFrom()->getId();
+                    $text = $update_content->getQuery();
+                }
+
+                printf(
+                    '%d: %s' . PHP_EOL,
+                    $chat_id,
+                    preg_replace('/\s+/', ' ', trim($text))
+                );
+            }
+        } else {
+            printf('Failed to fetch updates: %s' . PHP_EOL, $response->printError());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Handle the updates using the Webhook method.
+     *
+     * @throws \Exception
+     */
+    public function handleWebhook()
+    {
+        $this->telegram->handle();
+
+        return $this;
+    }
+
+    /**
+     * Set any extra bot features that have been assigned on construction.
+     */
+    public function setBotExtras()
+    {
+        isset($this->admins)         && $this->telegram->enableAdmins((array)$this->admins);
+        isset($this->mysql)          && $this->telegram->enableMySql($this->mysql);
+        isset($this->botan_token)    && $this->telegram->enableBotan($this->botan_token);
+        isset($this->commands_paths) && $this->telegram->addCommandsPaths((array)$this->commands_paths);
+        isset($this->custom_input)   && $this->telegram->setCustomInput($this->custom_input);
+        isset($this->download_path)  && $this->telegram->setDownloadPath($this->download_path);
+        isset($this->upload_path)    && $this->telegram->setUploadPath($this->upload_path);
+
+        if (isset($this->command_configs) && is_array($this->command_configs)) {
+            foreach ($this->command_configs as $command => $config) {
+                $this->telegram->setCommandConfig($command, $config);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ tests/BotManagerTest.php
@@ -0,0 +1,310 @@
+<?php
+/**
+ * This file is part of the TelegramBotManager package.
+ *
+ * (c) Armando Lüscher <armando@noplanman.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NPM\TelegramBotManager;
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
+
+/**
+ * Class BotManagerTest.php
+ *
+ * Leave all member variables public to allow easy modification.
+ *
+ * @package NPM\TelegramBotManager
+ */
+class BotManagerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var array Test vital parameters for BotManager.
+     */
+    private $vitalParams = [];
+
+    /**
+     * @var array Test optional parameters for BotManager.
+     */
+    private $optionalParams = [];
+
+    /**
+     * Set up BotManager instance.
+     */
+    public function setUp()
+    {
+        $this->vitalParams = [
+            'api_key' => 'api_key_12345',
+            'botname' => 'testbot',
+            'secret'  => 'secret_12345',
+        ];
+
+        $this->optionalParams = [
+            'webhook'         => 'https://php.telegram.bot/' . basename(__FILE__),
+            'selfcrt'         => __DIR__ . '/server.crt',
+            'admins'          => 1,
+            'mysql'           => ['host' => '127.0.0.1', 'user' => 'root', 'password' => 'root', 'database' => 'telegram_bot'],
+            'download_path'   => __DIR__ . '/Download',
+            'upload_path'     => __DIR__ . '/Upload',
+            'commands_paths'  => __DIR__ . '/CustomCommands',
+            'command_configs' => [
+                'weather'       => ['owm_api_key' => 'owm_api_key_12345'],
+                'sendtochannel' => ['your_channel' => '@my_channel'],
+            ],
+            'botan_token'     => 'botan12345',
+            'custom_input'    => '{"some":"raw", "json":"update"}',
+        ];
+    }
+
+    public function testSetParameters()
+    {
+        $botManager = new BotManager(array_merge($this->vitalParams, [
+           'param1' => 'param1value',
+           'param2' => 'param2value',
+           'param3' => 'param3value',
+        ]));
+        self::assertEquals('param1value', $botManager->param1);
+        self::assertEquals('param2value', $botManager->param2);
+        self::assertEquals('param3value', $botManager->param3);
+
+        self::assertObjectNotHasAttribute('param4', $botManager);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Some vital info is missing (api_key, botname or secret)
+     */
+    public function testNoVitalsFail()
+    {
+        new BotManager([]);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Some vital info is missing (api_key, botname or secret)
+     */
+    public function testSomeVitalsFail()
+    {
+        new BotManager(['api_key' => 'abc123', 'botname' => 'testbot']);
+    }
+
+    public function testVitalsSuccess()
+    {
+        new BotManager($this->vitalParams);
+    }
+
+    public function testMakeCliFriendlyInvalidParameterFormatFail()
+    {
+        $_SERVER['argv'] = [basename(__FILE__), 'invalid-param-format'];
+
+        $botManager = new BotManager($this->vitalParams);
+
+        self::assertEmpty($_GET);
+
+        $botManager->makeCliFriendly();
+
+        self::assertEmpty($_GET);
+    }
+
+    public function testMakeCliFriendlySuccess()
+    {
+        $botManager = new BotManager($this->vitalParams);
+
+        self::assertEmpty($_GET);
+
+        $_SERVER['argv'] = [basename(__FILE__)];
+        $botManager->makeCliFriendly();
+        self::assertEmpty($_GET);
+
+        $_SERVER['argv'] = [basename(__FILE__), 'l='];
+        $botManager->makeCliFriendly();
+        self::assertEquals(['l' => ''], $_GET);
+
+        $_GET = [];
+        $_SERVER['argv'] = [basename(__FILE__), 's=secret_12345', 'a=handle'];
+        $botManager->makeCliFriendly();
+        self::assertEquals(['s' => 'secret_12345', 'a' => 'handle'], $_GET);
+    }
+
+    public function testInitLogging()
+    {
+        $logging = [
+            'logging' => [
+                'debug'  => '/tmp/php-telegram-bot-debuglog.log',
+                'error'  => '/tmp/php-telegram-bot-errorlog.log',
+                'update' => '/tmp/php-telegram-bot-updatelog.log',
+            ]
+        ];
+        $botManager = new BotManager(array_merge($this->vitalParams, $logging));
+
+        self::assertFalse(TelegramLog::isDebugLogActive());
+        self::assertFalse(TelegramLog::isErrorLogActive());
+        self::assertFalse(TelegramLog::isUpdateLogActive());
+
+        $botManager->initLogging();
+
+        self::assertTrue(TelegramLog::isDebugLogActive());
+        self::assertTrue(TelegramLog::isErrorLogActive());
+        self::assertTrue(TelegramLog::isUpdateLogActive());
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid access
+     */
+    public function testValidateSecretFail()
+    {
+        $_GET = ['s' => 'NOT_my_secret_12345'];
+        $botManager = new BotManager(array_merge($this->vitalParams, ['secret' => 'my_secret_12345']));
+
+        $botManager->validateSecret();
+    }
+
+    public function testValidateSecretSuccess()
+    {
+        $_GET = ['s' => 'my_secret_12345'];
+        $botManager = new BotManager(array_merge($this->vitalParams, ['secret' => 'my_secret_12345']));
+
+        self::assertEquals($botManager, $botManager->validateSecret());
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid action
+     */
+    public function testValidateAndSetActionFailWithoutAction()
+    {
+        (new BotManager($this->vitalParams))->validateAndSetAction();
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid action
+     */
+    public function testValidateAndSetActionFailWithInvalidAction()
+    {
+        $_GET = ['a' => 'non-existent'];
+        (new BotManager($this->vitalParams))->validateAndSetAction();
+    }
+
+    public function testValidateAndSetActionSuccess()
+    {
+        $botManager = new BotManager($this->vitalParams);
+
+        $validActions = ['set', 'unset', 'reset', 'handle'];
+        foreach ($validActions as $action) {
+            $_GET = ['a' => $action];
+            self::assertEquals($action, $botManager->validateAndSetAction()->action);
+        }
+    }
+
+    public function testValidateAndSetWebhookSuccess()
+    {
+        $botManager = new BotManager(array_merge($this->vitalParams, ['webhook' => 'https://web/hook.php']));
+
+        $botManager->telegram = $this->getMockBuilder(Telegram::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['setWebHook', 'unsetWebHook', 'getDescription'])
+            ->getMock();
+        $botManager->telegram->expects(static::any())
+            ->method('setWebHook')
+            ->with('https://web/hook.php?a=handle&s=secret_12345')
+            ->will(static::returnSelf());
+        $botManager->telegram->expects(static::any())
+            ->method('unsetWebHook')
+            ->will(static::returnSelf());
+        $botManager->telegram->expects(static::any())
+            ->method('getDescription')
+            ->will(static::onConsecutiveCalls(
+            // set
+                'Webhook set',
+                'Webhook already set',
+                // unset
+                'Webhook deleted',
+                'Webhook does not exist',
+                // reset
+                'Webhook deleted',
+                'Webhook set'
+            ));
+
+        $botManager->action = 'set';
+        $botManager->validateAndSetWebhook();
+        self::assertSame('Webhook set', $botManager->test_output);
+
+        $botManager->validateAndSetWebhook();
+        self::assertSame('Webhook already set', $botManager->test_output);
+
+        $botManager->action = 'unset';
+        $botManager->validateAndSetWebhook();
+        self::assertSame('Webhook deleted', $botManager->test_output);
+
+        $botManager->validateAndSetWebhook();
+        self::assertSame('Webhook does not exist', $botManager->test_output);
+
+        $botManager->action = 'reset';
+        $botManager->validateAndSetWebhook();
+        self::assertSame('Webhook set', $botManager->test_output);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid webhook
+     */
+    public function testValidateAndSetWebhookFailSetWithoutWebhook()
+    {
+        $botManager = new BotManager(array_merge($this->vitalParams, ['webhook' => null]));
+        $botManager->action = 'set';
+        $botManager->validateAndSetWebhook();
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid webhook
+     */
+    public function testValidateAndSetWebhookFailResetWithoutWebhook()
+    {
+        $botManager = new BotManager(array_merge($this->vitalParams, ['webhook' => null]));
+        $botManager->action = 'reset';
+        $botManager->validateAndSetWebhook();
+    }
+
+    public function testIsAction()
+    {
+        $botManager = new BotManager($this->vitalParams);
+
+        $botManager->action = 'set';
+        self::assertTrue($botManager->isAction('set'));
+        self::assertTrue($botManager->isAction(['set']));
+        self::assertTrue($botManager->isAction(['set', 'handle']));
+
+        self::assertFalse($botManager->isAction('handle'));
+        self::assertFalse($botManager->isAction(['unset', 'reset']));
+    }
+
+    public function testGetLoopTime()
+    {
+        $botManager = new BotManager($this->vitalParams);
+
+        // Parameter not set.
+        self::assertSame(0, $botManager->getLoopTime());
+
+        $_GET = ['l' => 'text-string'];
+        self::assertSame(604800, $botManager->getLoopTime());
+
+        $_GET = ['l' => 0];
+        self::assertSame(604800, $botManager->getLoopTime());
+
+        $_GET = ['l' => -12345];
+        self::assertSame(604800, $botManager->getLoopTime());
+
+        $_GET = ['l' => 12345];
+        self::assertSame(12345, $botManager->getLoopTime());
+
+        $_GET = ['l' => '12345'];
+        self::assertSame(12345, $botManager->getLoopTime());
+    }
+}
